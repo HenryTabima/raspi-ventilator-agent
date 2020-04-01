189d8d3b5fbbde887efbedd202796ad278c8e269
Turista01 <62972706+Turista01@users.noreply.github.com>
2020-03-31 23:11:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const io = require('socket.io-client')
 
+const initSerialPort = require('./libs/serialport')
+
+const parser = initSerialPort()
+
+let pressure = 0
+let flow = 0
+
+parser.on('data', assignData)
+
+function toFloat(value){ 
+  return parseFloat(value)
+}
+
+function assignData(data){
+  data = data.split(',').map(toFloat)
+  flow = data[1]
+  pressure = data[0]
+
+}
+
 const socket = io('http://localhost:3000')
 
-let i = 0
+const INTERVAL_TIME = 250
 
 setInterval(() => {
   socket.emit('new data', {
     timestamp: new Date(),
-    pressure: Math.sin((Math.PI / 20) * i),
-    flow: 3 + Math.random()
+    pressure: pressure,
+    flow: flow
   })
-  i++
-}, 250)
+
+}, INTERVAL_TIME)
